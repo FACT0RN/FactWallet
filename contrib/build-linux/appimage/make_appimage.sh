2c24f469b508f1ab9f4fdab6a6827401c0fe39bf
Escanor Liones <ReverseControl@users.noreply.github.com>
2023-11-17 15:34:15 -0400

    [BUILD_SYSTEM] Add new dependencies: numpy and mpmath.

--- contrib/build-linux/appimage/make_appimage.sh
+++ contrib/build-linux/appimage/make_appimage.sh
@@ -164,7 +164,7 @@ info "Installing build dependencies."
 info "installing electrum and its dependencies."
 "$python" -m pip install --no-build-isolation --no-dependencies --no-binary :all: --no-warn-script-location \
     --cache-dir "$PIP_CACHE_DIR" -r "$CONTRIB/deterministic-build/requirements.txt"
-"$python" -m pip install --no-build-isolation --no-dependencies --no-binary :all: --only-binary PyQt5,PyQt5-Qt5,cryptography --no-warn-script-location \
+"$python" -m pip install --no-build-isolation --no-dependencies --no-binary :all: --only-binary PyQt5,PyQt5-Qt5,cryptography,sympy,mpmath --no-warn-script-location \
     --cache-dir "$PIP_CACHE_DIR" -r "$CONTRIB/deterministic-build/requirements-binaries.txt"
 "$python" -m pip install --no-build-isolation --no-dependencies --no-binary :all: --no-warn-script-location \
     --cache-dir "$PIP_CACHE_DIR" -r "$CONTRIB/deterministic-build/requirements-hw.txt"
