bcedf918e6b1c911d62e4873dcca806c9d1e7242
silkyjohnson1 <61355155+silkyjohnson1@users.noreply.github.com>
2024-02-23 16:56:38 -0500

    Update exchange_rate.py

--- electrum/exchange_rate.py
+++ electrum/exchange_rate.py
@@ -342,7 +342,7 @@ class CoinDesk(ExchangeBase):
 class CoinGecko(ExchangeBase):
 
     async def get_rates(self, ccy):
-        json = await self.get_json('api.coingecko.com', '/api/v3/coins/fact0rn?localization=False&sparkline=false')
+        json = await self.get_json('api.coingecko.com', '/api/v3/exchange_rates')
         return dict([(ccy.upper(), to_decimal(d['value']))
                      for ccy, d in json['rates'].items()])
 
@@ -352,7 +352,7 @@ class CoinGecko(ExchangeBase):
 
     async def request_history(self, ccy):
         history = await self.get_json('api.coingecko.com',
-                                      '/api/v3/coins/fact0rn/market_chart?vs_currency=%s&days=max' % ccy)
+                                      '/api/v3/coins/bitcoin/market_chart?vs_currency=%s&days=max' % ccy)
 
         return dict([(datetime.utcfromtimestamp(h[0]/1000).strftime('%Y-%m-%d'), str(h[1]))
                      for h in history['prices']])
