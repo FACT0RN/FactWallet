1edce7fe5efc2d43f92cd81220ba1ca9962c9d6b
silkyjohnson1 <61355155+silkyjohnson1@users.noreply.github.com>
2024-02-23 16:45:11 -0500

    Update exchange_rate.py

--- electrum/exchange_rate.py
+++ electrum/exchange_rate.py
@@ -297,7 +297,7 @@ class MEXC(ExchangeBase):
     async def get_rates(self, ccy):
         json = await self.get_json('api.mexc.com',
                              '/api/v3/ticker/price?symbol=FACTUSDT')
-        return {ccy: to_decimal(rate) for (ccy, rate) in json["price"].items()}
+        return {ccy: to_decimal(rate) for (ccy, rate) in json["price"]}
 
 class CoinCap(ExchangeBase):
 
