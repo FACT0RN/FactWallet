6975af05c51a0d3bf7affc2db2a7d3ccc6ca29ef
silkyjohnson1 <61355155+silkyjohnson1@users.noreply.github.com>
2024-02-24 10:46:35 -0500

    Update simple_config.py

--- electrum/simple_config.py
+++ electrum/simple_config.py
@@ -980,7 +980,7 @@ class SimpleConfig(Logger):
     )
 
     FX_USE_EXCHANGE_RATE = ConfigVar('use_exchange_rate', default=False, type_=bool)
-    FX_CURRENCY = ConfigVar('currency', default='EUR', type_=str)
+    FX_CURRENCY = ConfigVar('currency', default='USDT', type_=str)
     FX_EXCHANGE = ConfigVar('use_exchange', default='MEXC', type_=str)  # default exchange should ideally provide historical rates
     FX_HISTORY_RATES = ConfigVar(
         'history_rates', default=False, type_=bool,
