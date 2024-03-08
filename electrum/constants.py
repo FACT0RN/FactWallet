e01ceef1ab3da7836d0df58258d0eacb4b9ee10c
silkyjohnson1 <61355155+silkyjohnson1@users.noreply.github.com>
2024-03-07 19:42:07 -0500

    Update constants.py
    changed to 42069 as per 
    
    https://github.com/satoshilabs/slips/blob/master/slip-0044.md
    
    need to make sure this works

--- electrum/constants.py
+++ electrum/constants.py
@@ -99,7 +99,7 @@ class BitcoinMainnet(AbstractNet):
         'p2wsh':       0x02aa7ed3,  # Zpub
     }
     XPUB_HEADERS_INV = inv_dict(XPUB_HEADERS)
-    BIP44_COIN_TYPE = 0
+    BIP44_COIN_TYPE = 42069
     LN_REALM_BYTE = 0
     LN_DNS_SEEDS = [
     ]
